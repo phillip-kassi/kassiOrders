99bbcab10410fbd1d8232afa01bf4623c1d39db4
phillip-kassi <kassiorders@gmail.com>
2019-10-30 20:08:32 +0100

    add new stuff

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var path = require('path');
 
 //when i'm conneting to remote database
 mongoose.connect('mongodb://admin:admin1234%2A%2A@cluster0-shard-00-00-xbnnw.mongodb.net:27017,cluster0-shard-00-01-xbnnw.mongodb.net:27017,cluster0-shard-00-02-xbnnw.mongodb.net:27017/kassApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true , useUnifiedTopology: true }).then(() =>{
-  console.log('connected to database');
+  console.log('connected to database 4');
 })
 .catch(() => {
   console.log('could not connect to database :(');
@@ -25,9 +25,9 @@ var app = express();
 app.use(cors());
 app.use(bodyparser.json());
 app.use('/api', router);
-app.use(express.static(path.join(__dirname, "kass-order-app")));
+app.use(express.static(path.join(__dirname, "public")));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "kass-order-app/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 })
 app.use(pdf);
 
